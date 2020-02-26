244562fbe2ae3d1b61caa1544abfc28f36d9d2c1
Vinícius Lima <vinizge@gmail.com>
2020-02-26 17:12:04 -0300

    Error handler, slugfying names and Geocode API

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,83 +1,72 @@
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
 const Bootcamp = require('../models/bootcamp');
 
 //@desc   Get all bootcamps
 //@route  GET /api/v1/bootcamps
 //@access Public
-exports.getBootCamps = async (req, res, next) => {
-  try {
-    const bootcamps = await Bootcamp.find();
-    res.status(200).json({success: true, data: bootcamps});
-  } catch (err) {
-    res.status(400).json({success: false});
-  }
-}
+exports.getBootCamps = asyncHandler( async (req, res, next) => {
+  const bootcamps = await Bootcamp.find();
+
+  res.status(200).json({success: true, count: bootcamps.length, data: bootcamps});
+ 
+});
 
 //@desc   Get single bootcamp
 //@route  GET /api/v1/bootcamps/:id
 //@access Public
-exports.getBootCamp = async (req, res, next) => {
-  try {
+exports.getBootCamp = asyncHandler(async (req, res, next) => {
+ 
     const bootcamp = await Bootcamp.findById(req.params.id);
     if(!bootcamp){
-      return res.status(400).json({success: false});
+      return next(new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404));
     }
     res.status(200).json({success: true, data: bootcamp});
-  } catch (err) {
-    res.status(400).json({success: false});
-  }  
-}
+    
+});
 
 //@desc   Create new bootcamp
 //@route  POST /api/v1/bootcamps
 //@access Private
-exports.createBootCamp = async (req, res, next) => {
- try {
+exports.createBootCamp = asyncHandler(async (req, res, next) => {
+ 
   const bootcamp = await Bootcamp.create(req.body);
   res.status(201).json({
     success: true,
     data: bootcamp
  });
-}catch (err) {
-  res.status(400).json({success: false});
- } 
  
-};
+ 
+});
 
 //@desc   Update bootcamps
 //@route  PUT /api/v1/bootcamps/:id
 //@access Private
-exports.updateBootCamp = async (req, res, next) => {
-  try {
+exports.updateBootCamp = asyncHandler(async (req, res, next) => {
+  
     const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body,{
       new: true,
       runValidators: true
     });
   
     if(!bootcamp){
-      return res.status(400).json({success: false});
+      return next(new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404));
     }
     
     res.status(200).json({success: true, data: bootcamp});
-  } catch (error) {
-    res.status(400).json({success: false});
-  }
-  
-  
-}
+    
+});
 
 //@desc   Delete bootcamp
 //@route  DELETE /api/v1/bootcamps/:id
 //@access Private
-exports.deleteBootCamp = async (req, res, next) => {
-  try {
+exports.deleteBootCamp = asyncHandler( async (req, res, next) => {
+  
     const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
   
     if(!bootcamp){
-      return res.status(400).json({success: false});
+      return next(new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404));
     }
     
     res.status(200).json({success: true, message: `Object ID ${req.params.id} was successfully deleted !` });
-  } catch (error) {
-    res.status(400).json({success: false});
-  }
-}
+});
---
+++ utils/geocoder.js
@@ -0,0 +1,14 @@
+const NodeGeocoder = require('node-geocoder');
+
+const options = {
+  provider: process.env.GEOCODER_PROVIDER,
+  httpAdapter: 'https',
+  apiKey: process.env.GEOCODER_API_KEY,
+  formatter: null
+};
+
+console.log(options.provide, options.apiKey);
+
+const geocoder = NodeGeocoder(options);
+
+module.exports = geocoder;
